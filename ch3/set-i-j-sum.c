c9c709b07dcca9ab30d0aae876fec8d643611555
hongjyunwang <jkwg423@gmail.com>
2021-09-25 16:42:33 +0800

    Add ch3/set-i-j-sum

---
+++ ch3/set-i-j-sum.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i, j;
+    scanf("%d%d", &i, &j);
+    int k = i + j;
+    printf("%d\n", k);
+    k = i - j;
+    printf("%d\n", k);
+
+    system("pause");
+    return 0;
+}
