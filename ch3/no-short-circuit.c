00e5a2834575963aec568189567cf8006c45a3af
hongjyunwang <jkwg423@gmail.com>
2021-09-26 15:45:14 +0800

    Add ch3/no-short-circuit

---
+++ ch3/no-short-circuit.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i, j;
+    scanf("%d%d", &i, &j);
+
+    int k = 3, l = 4;
+    k = i;
+    l = j;
+    if (k > 0 || l > 0)
+    {
+        printf("%d\n", k);
+        printf("%d\n", l);
+    }
+
+    system("pause");
+    return 0;
+}
