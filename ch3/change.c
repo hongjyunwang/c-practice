49519b1cc42793be8544a42609363711b8bbd5ed
hongjyunwang <jkwg423@gmail.com>
2021-09-26 10:09:30 +0800

    Add ch3/change

---
+++ ch3/change.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int change;
+    scanf("%d", &change);
+    int fifty = change / 50;
+    change -= fifty * 50;
+    int ten = change / 10;
+    change -= ten * 10;
+    int five = change / 5;
+    change -= five * 5;
+    int one = change;
+
+    printf("%d\n", fifty);
+    printf("%d\n", ten);
+    printf("%d\n", five);
+    printf("%d\n", one);
+
+    system("pause");
+    return 0;
+}
