670788f713afead6bc725a127df568ef7b90fd45
hongjyunwang <jkwg423@gmail.com>
2021-09-27 10:34:05 +0800

    Add ch4/cond-max-3

---
+++ ch4/cond-max-3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i, j, k;
+    scanf("%d%d%d", &i, &j, &k);
+    int max = (i > j) ? i : j;
+    if (k > max)
+        max = k;
+    printf("%d\n", max);
+
+    system("pause");
+    return 0;
+}
