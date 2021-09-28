4a2f9540e48cf5363470ce1e0bfc378a5e83e79c
hongjyunwang <jkwg423@gmail.com>
2021-09-28 23:09:30 +0800

    Add ch6/print-array

---
+++ ch6/print-array.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define ARRAYSIZE 10
+
+int main()
+{
+    int a[ARRAYSIZE];
+    for (int i = 0; i < ARRAYSIZE; i++)
+        scanf("%d", &(a[i]));
+    for (int i = 0; i < ARRAYSIZE; i++)
+        printf("%d\n", a[i]);
+
+    system("pause");
+    return 0;
+}
