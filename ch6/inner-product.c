02ea565976913eb50ec82a4dc4a4786874acf4c2
hongjyunwang <jkwg423@gmail.com>
2021-09-28 23:17:11 +0800

    Add ch6/inner-product

---
+++ ch6/inner-product.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define ARRAYSIZE 5
+
+int main()
+{
+    int A[ARRAYSIZE], B[ARRAYSIZE];
+    for (int i = 0; i < ARRAYSIZE; i++)
+        scanf("%d", &(A[i]));
+    for (int i = 0; i < ARRAYSIZE; i++)
+        scanf("%d", &(B[i]));
+    int C = 0;
+    for (int i = 0; i < ARRAYSIZE; i++)
+        C += A[i] * B[i];
+    printf("%d\n", C);
+
+    system("pause");
+    return 0;
+}
