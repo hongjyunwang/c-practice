d33aefbc6ff16c14b021bc0ac2b3b5a1194f9819
hongjyunwang <jkwg423@gmail.com>
2021-09-29 17:04:55 +0800

    Add ch6/print-array-address-exp

---
+++ ch6/print-array-address-exp.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define ARRAYSIZE 10
+
+int main()
+{
+    int a[ARRAYSIZE];
+    printf("&a is %p\n", &a);
+    printf("a is %p\n", a);
+    printf("&(a[0]) is %p\n", &(a[0]));
+    printf("a + 3 is %p\n", a + 3);
+    printf("&(a[3]) is %p\n", &(a[3]));
+
+    system("pause");
+    return 0;
+}
